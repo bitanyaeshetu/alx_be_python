54425ff39070b14193f1d3b9ce9e3af1ff2ff974
bitanyaeshetu1@gmail.com <bitanyaeshetu1@gmail.com>
2024-10-09 06:06:25 -0700

    add bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_holder = 0, ):
+        self.account_holder = account_holder
+        
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_holder:
+            self.account_holder -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_holder:.2f}")  # Ensure two decimal places and no extra spaces
