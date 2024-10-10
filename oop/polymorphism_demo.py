1e2302b6d2713edd9e21604f7d7266949d5cfe0f
bitanyaeshetu1@gmail.com <bitanyaeshetu1@gmail.com>
2024-10-10 04:55:41 -0700

    Add polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,17 @@
+# polymorphism_demo.py
+
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must override this method")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width  # Area of rectangle
+
+class Ci
