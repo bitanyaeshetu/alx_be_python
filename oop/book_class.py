42c1d6a8270ab510d42b9129943ebd8b39239495
bitanyaeshetu1@gmail.com <bitanyaeshetu1@gmail.com>
2024-10-10 01:41:29 -0700

    Add book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,27 +1,27 @@
+print("Script started")
+
 class Book:
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
-    # String representation for human-readable format
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    # Official representation to recreate the object
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    # Destructor to print a message when the object is deleted
     def __del__(self):
         print(f"Deleting {self.title}")
 
-# Create a book instance
+print("Creating book instance")
 book = Book("1984", "George Orwell", 1949)
 
-# Print book details using __str__ and __repr__
-print(book)        # Expected: 1984 by George Orwell, published in 1949
-print(repr(book))  # Expected: Book('1984', 'George Orwell', 1949)
+print("Printing book details")
+print(book)
+print(repr(book))
 
-# Delete the book instance
+print("Deleting book instance")
 del book
+print("Script ended")
