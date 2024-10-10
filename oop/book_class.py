7a51175bc70bc7b423822830d2f801e0cca5ce88
bitanyaeshetu1@gmail.com <bitanyaeshetu1@gmail.com>
2024-10-10 01:51:09 -0700

    Add book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,20 @@
 class Book:
     def __init__(self, title, author, year):
+        """Initializes a Book instance with title, author, and year."""
         self.title = title
         self.author = author
         self.year = year
 
-    # String representation for human-readable format
     def __str__(self):
+        """Returns a string in the format 'title by author, published in year'."""
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    # Official representation to recreate the object
     def __repr__(self):
+        """Returns a string that would recreate the Book instance."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    # Destructor to print a message when the object is deleted
     def __del__(self):
+        """Prints 'Deleting (title of the book)' upon object deletion."""
         print(f"Deleting {self.title}")
 
 # Create a book instance
