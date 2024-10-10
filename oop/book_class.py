c08b50e3cd14f642933f95bcde111fa382e7b06c
bitanyaeshetu1@gmail.com <bitanyaeshetu1@gmail.com>
2024-10-10 01:28:04 -0700

    Add book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,11 @@
-# book_class.py
-
 class Book:
     def __init__(self, title, author, year):
-        """Constructor to initialize the book's attributes."""
         self.title = title
         self.author = author
         self.year = year
 
-    def __del__(self):
-        """Destructor that prints a message when the object is deleted."""
-        print(f"Deleting {self.title}")
-
-    def __str
+    # The __str__ method should return a readable string for the user
+    def __str__(self):
+        return f"'{self.title}' by {self.author} ({self.year})"
+    
+    # The __repr_
